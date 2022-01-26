c56dd64c2e288784cacf14e5f02241890adad7f9
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-26 18:48:00 -0500

    Setup email sending

--- src/settings.py
+++ src/settings.py
@@ -38,6 +38,12 @@ if DEBUG:
 else:
     ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 # Application definition
 

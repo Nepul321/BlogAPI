52941c8fb3201230564864ae239137813fd4aadf
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-27 19:09:43 -0500

    Update UserKey db model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,6 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 class UserKey(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     activated = models.BooleanField(default=False)
     key = models.CharField(max_length=255, default='')

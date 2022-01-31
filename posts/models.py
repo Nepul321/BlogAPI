ea86132f6061bf3771e788c16e632c8fb4152aad
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-31 16:55:26 -0500

    Create DB model Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)

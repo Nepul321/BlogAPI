b3b0651858f3f35fe96edaefe630d8ad772a546f
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-04 20:04:37 -0500

    Create serializers for comments

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import (
+    Comment
+)
 
 @api_view(['GET'])
 def CommentListView(request):
+    qs = Comment.objects.all()
     return Response({}, status=200)

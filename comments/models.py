5e4853ccc0c8576cf6a86e8ef41c9a9700b46f99
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-04 19:33:48 -0500

    Create Comment DB model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
+from base.models import User
 from django.db import models
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="comment_likes")
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime']

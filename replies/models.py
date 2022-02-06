21b1977e624626b331c6ed568257700d619e77c4
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-05 20:08:25 -0500

    Create Reply DB model

--- replies/models.py
+++ replies/models.py
@@ -1,3 +1,14 @@
+from base.models import User
 from django.db import models
+from comments.models import Comment
 
-# Create your models here.
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="reply_likes")
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime']

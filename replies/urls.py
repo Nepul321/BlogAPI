2091810b9f792ed537cabbb195975869a69bd2c0
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-05 20:19:40 -0500

    Create a Comment Replies view

--- replies/urls.py
+++ replies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
+    CommentRepliesView,
     ReplyListView
 )
 
 urlpatterns = [
     path('', ReplyListView, name="reply-list"),
+    path('comment/<int:id>/', CommentRepliesView, name="comment-replies-list")
 ]
--- replies/views.py
+++ replies/views.py
@@ -8,9 +8,26 @@ from .models import (
     Reply
 )
 
+import jwt
+
+from comments.models import Comment
+
+from base.models import User
+
 @api_view(['GET'])
 def ReplyListView(request):
     qs = Reply.objects.all()
     serializer = ReplySerializer(qs, many=True)
+    data = serializer.data
+    return Response(data, status=200)
+
+@api_view(['GET'])
+def CommentRepliesView(request, id):
+    commentQs = Comment.objects.filter(id=id)
+    if not commentQs:
+        return Response({"detail" : "Comment not found"}, status=404)
+    obj = commentQs.first()
+    replyQs = Reply.objects.filter(comment=obj)
+    serializer = ReplySerializer(replyQs, many=True)
     data = serializer.data
     return Response(data, status=200)

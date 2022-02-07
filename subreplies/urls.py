7ab56a8fa24efcd2d796cb381cd0c7b1f57c3af2
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-07 17:42:50 -0500

    Create a view with the sub replies for every reply

--- subreplies/urls.py
+++ subreplies/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
 from .views import (
-    SubreplylistView
+    SubreplylistView,
+    ReplySubRepliesView
 )
 
 urlpatterns = [
-    path('', SubreplylistView, name="sub-reply-list-view")
+    path('', SubreplylistView, name="sub-reply-list-view"),
+    path('replies/<int:id>/', ReplySubRepliesView, name="reply-subreply-view"),
 ]
--- subreplies/views.py
+++ subreplies/views.py
@@ -4,6 +4,8 @@ from .models import (
     SubReply
 )
 
+from replies.models import Reply
+
 from .serializers import (
     SubReplyActionSerializer,
     SubReplySerializer
@@ -13,5 +15,16 @@ from .serializers import (
 def SubreplylistView(request):
     qs = SubReply.objects.all()
     serializer = SubReplySerializer(qs, many=True)
+    data = serializer.data
+    return Response(data, status=200)
+
+@api_view(['GET'])
+def ReplySubRepliesView(request, id):
+    replyQs = Reply.objects.filter(id=id)
+    if not replyQs:
+        return Response({"detail" : "Comment not found"}, status=404)
+    obj = replyQs.first()
+    subreplyQs = SubReply.objects.filter(reply=obj)
+    serializer = SubReplySerializer(subreplyQs, many=True)
     data = serializer.data
     return Response(data, status=200)

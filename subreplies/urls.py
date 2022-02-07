9d40ef074222b9bd4b57fd2714ecb8bde0dc6281
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-07 17:57:22 -0500

    Create a sub reply like && unlike view

--- subreplies/urls.py
+++ subreplies/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     SubReplyDetailDeleteView,
     SubreplylistView,
-    ReplySubRepliesView
+    ReplySubRepliesView,
+    SubReplyLikeUnlikeView
 )
 
 urlpatterns = [
     path('', SubreplylistView, name="sub-reply-list-view"),
     path('replies/<int:id>/', ReplySubRepliesView, name="reply-subreply-view"),
     path('<int:id>/', SubReplyDetailDeleteView, name="sub-reply-detail-delete-view"),
+    path('action/', SubReplyLikeUnlikeView, name="sub-reply-like-unlike-view"),
 ]
--- subreplies/views.py
+++ subreplies/views.py
@@ -15,6 +15,8 @@ import jwt
 
 from base.models import User
 
+from users.decorators import login_required
+
 @api_view(["GET"])
 def SubreplylistView(request):
     qs = SubReply.objects.all()
@@ -55,3 +57,30 @@ def SubReplyDetailDeleteView(request, id):
     serializer = SubReplySerializer(obj)
     data = serializer.data
     return Response(data, status=200)
+
+@api_view(['POST'])
+@login_required
+def SubReplyLikeUnlikeView(request):
+    serializer = SubReplyActionSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.validated_data
+    id = data.get("id")
+    action = data.get("action")
+    qs = SubReply.objects.filter(id=id)
+    if not qs:
+        return Response({"detail" : "Sub Reply does not exist"}, status=404)
+    obj = qs.first()
+    token = request.COOKIES.get("jwt")
+    payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+    user = User.objects.filter(id=payload['id']).first()
+
+    if action == "like":
+        obj.likes.add(user)
+        serializer = SubReplySerializer(obj)
+        return Response(serializer.data, status=200)
+    elif action == "unlike":
+        obj.likes.remove(user)
+        serializer = SubReplySerializer(obj)
+        return Response(serializer.data, status=200)
+
+    return Response({}, status=401)

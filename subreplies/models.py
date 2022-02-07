e7690de6d283a3d2aa557737dd043c5d13b9ef52
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-07 17:29:01 -0500

    Create SubReply DB model

--- subreplies/models.py
+++ subreplies/models.py
@@ -1,3 +1,14 @@
+from base.models import User
 from django.db import models
+from replies.models import Reply
 
-# Create your models here.
+class SubReply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="subreply_likes")
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime']
